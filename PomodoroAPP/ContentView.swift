b116bde61ab55183758cae553c65a75eedcbf8ea
Valerii Gassiev <vvv.gassiev@gmail.com>
2023-10-11 22:41:49 +0300

    Initial Commit

---
+++ PomodoroAPP/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PomodoroAPP
+//
+//  Created by Valera Gassiev on 10/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PomodoroAPP/PomodoroAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroAPPApp.swift
+//  PomodoroAPP
+//
+//  Created by Valera Gassiev on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
